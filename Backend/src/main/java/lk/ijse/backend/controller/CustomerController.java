408fc6808b7d7ed6ea5c4a3e59018b3b9da40d2c
kavindisathsarani <dksmanimendra0@gmail.com>
2025-03-07 19:05:16 +0530

    testing

--- Backend/src/main/java/lk/ijse/backend/controller/CustomerController.java
+++ Backend/src/main/java/lk/ijse/backend/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/customer")
 @CrossOrigin(origins = "*")
 public class CustomerController {
+    //testing
     @GeneratedValue
     @Autowired
     private CustomerServiceImpl customerServiceImpl;
